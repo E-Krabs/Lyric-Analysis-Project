633b0cedd543195f0436b1cee54b00425dfc5ac1
Fast_and_Fuhrerious <Tyler.Smith11407@gmail.com>
2021-10-13 15:33:17 -0400

    Update pie.py

--- pie.py
+++ pie.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-y = np.array([899, 817, 750, 745])
-mylabels = ["love", "oh", "god", "you're"]
+y = np.array([])
+mylabels = []
 myexplode = [0.2, 0, 0, 0]
 
 plt.pie(y, labels = mylabels, explode=myexplode, autopct='%1.2f%%')
