f0d1a0e1a71438ef289324e964a7ac79fd1735d7
Fast_and_Fuhrerious <Tyler.Smith11407@gmail.com>
2021-10-13 15:36:43 -0400

    Update line.py

--- line.py
+++ line.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
    
 year = [2017, 2018, 2019, 2020]
-love = [267, 212, 240, 180]
-oh = [373, 184, 234, 238]
+x = []
+y = []
 
-plt.plot(year, love, color='red', marker='o', label='love')
-plt.plot(year, oh, marker='o', label='oh')
+plt.plot(year, x, color='red', marker='o', label='x')
+plt.plot(year, y, marker='o', label='y')
 
-plt.title('Love Trend (2017-2020)')
+plt.title('TITLE')
 plt.xlabel('Year')
 plt.ylabel('Count')
 plt.grid()
